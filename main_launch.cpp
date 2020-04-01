92988bd15363a57f2c78f172c1939d3609a649bc
RazvanMelecciu <razvan.melecciu@gmail.com>
2020-04-01 11:31:49 -0700

    Added debug release config

--- main_launch.cpp
+++ main_launch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <filesystem>
 
 #include "main_launch.hpp"
 #include "config.h"
@@ -11,5 +12,13 @@ int main(int argc, char** argv)
                                                        %BOOST_VER_MAJOR 
                                                        %BOOST_VER_MINOR 
                                                        %BOOST_VER_PATCH;
+    auto crtPath = std::filesystem::current_path();
+    LOG("Current directory: %s") %crtPath;
+    LOG("No. passed parameters: %d") %argc;
+    for (auto i = 0; i < argc; ++i)
+    {
+        LOG("Param %d >> %s") %i %argv[i];
+    }
+
     return 0;
 }
